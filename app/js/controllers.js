561167899ac18fba70e38a415e885054993e6e61
thofis <tf@media-nox.de>
2013-11-01 14:27:16 +0100

    providing searchTerms

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,8 +1,31 @@
 'use strict';
-
 /* Controllers */
 
 angular.module('hangularApp.controllers', []).
-   controller('HangularCtrl', [function() {
+   controller('HangularCtrl',
+      ['$scope', 'wordUtils', 'wordProvider',
+          function($scope, wordUtils, wordProvider) {
 
-       }]);
+                
+
+              $scope.gui = {};
+              
+              $scope.gui.images = [
+                  {path: "images/hangman_0.png"},
+                  {path: "images/hangman_1.png"},
+                  {path: "images/hangman_2.png"},
+                  {path: "images/hangman_3.png"},
+                  {path: "images/hangman_4.png"},
+                  {path: "images/hangman_5.png"},
+                  {path: "images/hangman_6.png"},
+                  {path: "images/hangman_7.png"}
+              ];
+              
+              var setWords = function(word) {
+                $scope.gui.searchterm = word;
+                $scope.gui.hiddenSearchterm = wordUtils.convertToHidden(word);                  
+              };
+              
+              wordProvider.getRandomWord(setWords);
+              
+          }]);
--- app/js/services.js
+++ app/js/services.js
@@ -2,8 +2,43 @@
 
 /* Services */
 
-
-// Demonstrate how to register services
-// In this case it is a simple value service.
 angular.module('hangularApp.services', []).
-   value('version', '0.1');
+   value('alphabet', 'abcdefghijklmnopqrstuvwxyz').
+   service('wordUtils', ['alphabet', function(alphabet) {
+
+           this.isLetter = function(ch) {
+               return alphabet.indexOf(ch.toLowerCase()) >= 0;
+           };
+
+           this.convertToHidden = function(term) {
+               var hidden = "";
+               for (var i = 0; i < term.length; i++) {
+                   var ch = term.charAt(i);
+                   if (this.isLetter(ch)) {
+                       hidden += "-";
+                   } else {
+                       hidden += ch;
+                   }
+               }
+               return hidden;
+           };
+       }]).
+   factory('wordProvider', ['$http', function($http) {
+           var wp = {};
+
+           var callbackFunction;
+
+           var provideWord = function(data, status, headers, config) {
+               var countries = data;
+               var randomIndex = Math.floor(Math.random() * countries.length);
+               var word = countries[randomIndex];
+               callbackFunction(word);
+           };
+
+           wp.getRandomWord = function(fn) {
+               callbackFunction = fn;
+               $http.get('resources/countries.json').success(provideWord);
+           };
+
+           return wp;
+       }]);
